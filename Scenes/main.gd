4f36e238a3b1160a8c3043e7cad02943146e8102
Rigo <217056768+Recsaur@users.noreply.github.com>
2026-02-02 19:12:44 +0400

    Changes

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -8,8 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	pass	
 
 func _on_pause_pressed() -> void:
 	if get_tree().paused == true:	
